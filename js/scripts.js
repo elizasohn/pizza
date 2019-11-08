b55974a176b293e6a8568414eb6bf8bce377a52b
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-08 10:41:26 -0800

    add checkbox forms and futile jquery attempts to get inputs

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,61 @@ function Pizza (size, toppings) {
   this.price = 0;
 }
 
-Pizza.prototype.getPrice = function (){
-  this.toppings.forEach(function(veggie){
+Pizza.prototype.getPrice = function(){
+  this.toppings.forEach(function(topping){
     this.price += 1;
-  }
+  });
   if (this.size === 16){
-    this.price += 4;
-  }else if (this size === 14){
-    this.price += 2;
-  }else if (this size === 12){
-    this.price += 0;
+    this.price += 16;
+  }else if (this.size === 14){
+    this.price += 14;
+  }else if (this.size === 12){
+    this.price += 10;
   }
   return this.price;
-});
+};
 
 
 
 
 //User Interface Logic
 $(document).ready(function(){
-  $("#submitBtn").submit(function(event){
-    event.preventDefault;
-    let toppingsInputs = $("#toppings:checked");
-    let toppingsArray = [];
-    let sizeInput = $('#size').val();
-    toppingsInputs.forEach(function(toppingInput){
-      toppingsArray.push(veg.Input.val());
-    });
+  $("form#pizzaOrder").submit(function(event){
+    event.preventDefault();
+    var toppingsArray = [];
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppingsInputs = $(this).val();
+      toppingsArray.push(toppingsInputs);
+    })
+    console.log(toppingsInputs);
+    console.log(toppingsArray);
 
-    let myPizza = new Pizza(sizeInput, toppingsArray);
-    let price = myPizza.getPrice();
+    // var sizeInput = $('.size').val();
+    // toppingsInputs.each(function(toppingInput){
+    //   toppingsArray.push(toppingsInputs.val());
+    // });
+    // var myPizza = new Pizza(sizeInput, toppingsArray);
+    // var price = myPizza.getPrice();
   })
 })
+
+
+
+
+
+// $(document).ready(function(){
+//   $("form#transportation_survey").submit(function(event){
+//     event.preventDefault();
+//     $("#work-responses").show();
+//     $("input:checkbox[name=work-transportation]:checked").each(function(){
+//       var workTransportationMode = $(this).val();
+//       $('#work-responses').append(workTransportationMode + "<br>");
+//     });
+//     $("#fun-responses").show();
+//     $("input:checkbox[name=fun-transportation]:checked").each(function(){
+//       var funTransportationMode = $(this).val();
+//       $('#fun-responses').append(funTransportationMode + "<br>");
+//     });
+//     $('#transportation_survey').hide();
+//   });
+// });
